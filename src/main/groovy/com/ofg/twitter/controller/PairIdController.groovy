cbff19c995b903d92353dd14664750e4d7f5dc0e
Dariusz Stefanski <d.a.stefanski@gmail.com>
2014-09-26 18:41:31 +0200

    Controller mock

--- src/main/groovy/com/ofg/twitter/controller/PairIdController.groovy
+++ src/main/groovy/com/ofg/twitter/controller/PairIdController.groovy
@@ -1,9 +1,12 @@
 package com.ofg.twitter.controller
+
 import com.ofg.twitter.controller.place.extractor.PropagationWorker
 import com.wordnik.swagger.annotations.Api
 import com.wordnik.swagger.annotations.ApiOperation
+import groovy.transform.ToString
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,21 +19,32 @@ import static org.springframework.web.bind.annotation.RequestMethod.PUT
 
 @Slf4j
 @RestController
-@RequestMapping('/api')
+@RequestMapping('/api/loanApplication')
 @Api(value = "pairId", description = "Collects places from tweets and propagates them to Collerators")
 class PairIdController {
 
     @Autowired private PropagationWorker propagationWorker
-    
+
+
     @RequestMapping(
-            value = '{pairId}',
+            value = '/{loanApplicationId}',
             method = PUT,
-            consumes = 'application/vnd.com.ofg.twitter-places-analyzer.v1+json',
-            produces = 'application/vnd.com.ofg.twitter-places-analyzer.v1+json')
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation(value = "Async collecting and propagating of tweets for a given pairId",
             notes = "This will asynchronously call tweet collecting, place extracting and their propagation to Collerators")
-    Callable<Void> getPlacesFromTweets(@PathVariable @NotNull long pairId, @RequestBody @NotNull String tweets) {
-        propagationWorker.collectAndPropagate(pairId, tweets)
-    }
+    Callable<Void> getPlacesFromTweets(@PathVariable("loanApplicationId") @NotNull long loanApplicationId,
+                                       @RequestBody @NotNull LoanApplication loanApplication) {
 
+//        propagationWorker.collectAndPropagate(pairId, tweets)
+        log.info("Loan application request: {}, id: {}", loanApplication.toString(), loanApplicationId)
+    }
 }
+@ToString
+class LoanApplication {
+    String firstName;
+    String lastName;
+    String job;
+    BigDecimal amount;
+}
+
