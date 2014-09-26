7bc3ee0da15781b2fbe5a980b443fe2f8a731e36
Michał Niczyporuk <m.niczyporuk@polish-airports.com>
2014-09-26 19:17:04 +0200

    add decision logics

--- src/main/groovy/com/ofg/twitter/controller/PairIdController.groovy
+++ src/main/groovy/com/ofg/twitter/controller/PairIdController.groovy
@@ -1,21 +1,21 @@
 package com.ofg.twitter.controller
 
 import com.ofg.infrastructure.web.resttemplate.fluent.ServiceRestClient
-import com.ofg.twitter.controller.place.extractor.PropagationWorker
 import com.wordnik.swagger.annotations.Api
 import com.wordnik.swagger.annotations.ApiOperation
 import groovy.json.JsonOutput
 import groovy.transform.ToString
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 import javax.validation.constraints.NotNull
-import java.util.concurrent.Callable
 
 import static org.springframework.web.bind.annotation.RequestMethod.PUT
 
@@ -24,10 +24,8 @@ import static org.springframework.web.bind.annotation.RequestMethod.PUT
 @RequestMapping('/api/loanApplication')
 @Api(value = "pairId", description = "Collects places from tweets and propagates them to Collerators")
 class PairIdController {
-
-    @Autowired private PropagationWorker propagationWorker
-
-    @Autowired private ServiceRestClient serviceRestClient
+    @Autowired
+    private ServiceRestClient serviceRestClient
 
 
     private static final DECISION_MAKER = "decision-make"
@@ -40,14 +38,14 @@ class PairIdController {
             produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation(value = "Async collecting and propagating of tweets for a given pairId",
             notes = "This will asynchronously call tweet collecting, place extracting and their propagation to Collerators")
-    Callable<Void> getPlacesFromTweets(@PathVariable("loanApplicationId") @NotNull long loanApplicationId,
-                                       @RequestBody @NotNull LoanApplication loanApplication) {
+    ResponseEntity<Object> getPlacesFromTweets(@PathVariable("loanApplicationId") @NotNull long loanApplicationId,
+                                               @RequestBody @NotNull LoanApplication loanApplication) {
 
         log.info("Loan application request: {}, id: {}", loanApplication.toString(), loanApplicationId)
 
         // TODO(dst): add logic here
-        loanApplication.fraudStatus = "SOME_STATUS"
-
+        loanApplication.fraudStatus = determineClientType(loanApplication)
+        log.info("client status is {}", loanApplication.fraudStatus)
 
         serviceRestClient.forService(DECISION_MAKER)
                 .post()
@@ -58,13 +56,37 @@ class PairIdController {
                 .andExecuteFor()
                 .anObject()
                 .ofType(String)
+        return new ResponseEntity<Object>(HttpStatus.OK)
+    }
+
+    private ClientType determineClientType(LoanApplication loanApplication) {
+        if (loanApplication.job == "OTHER" ||
+                loanApplication.amount > 2000 ||
+                loanApplication.lastName?.length() < 2) {
+            return ClientType.FRAUD
+        } else if ("FINANCE SECTOR" == loanApplication.job ||
+                (loanApplication.amount > 1000 && loanApplication.amount < 2000) ||
+                loanApplication.lastName?.length() > 25) {
+            return ClientType.FISHY
+        } else if ("IT" == loanApplication.job ||
+                loanApplication.amount < 2000 ||
+                loanApplication.lastName?.length() < 2) {
+            return ClientType.GOOD
+        }
     }
 }
+
 @ToString
 class LoanApplication {
     String firstName
     String lastName
     String job
     BigDecimal amount
-    String fraudStatus
+    ClientType fraudStatus
+}
+
+enum ClientType {
+    FRAUD,
+    FISHY,
+    GOOD;
 }
